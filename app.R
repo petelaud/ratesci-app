2a89932917be7f7bdec97ff7c821c9e188911903
Pete Laud <p.j.laud@shef.ac.uk>
2026-03-23 22:43:50 +0000

    improve Shiny app stability & use radio buttons

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(ratesci)
 
@@ -8,129 +7,219 @@ ui <- pageWithSidebar(
   # Application title
   headerPanel("Skewness-Corrected Asymptotic Score confidence intervals for comparison of rates"),
 
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
+  # Sidebar with controls to select the distribution and contrast etc
   sidebarPanel(
-    selectInput("dist", "Distribution:",
-                list("Binomial" = "bin",
-                     "Poisson" = "poi")),
+
+    numericInput("level",
+              "Confidence level (%):",
+              95,
+              min = 1,
+              max = 100),
+
+    radioButtons("dist", "Distribution:",
+                 list("Binomial" = "bin",
+                      "Poisson" = "poi"),
+                 selected = "bin",
+                 inline = TRUE),
+
     conditionalPanel('input.dist == "bin"',
-                     selectInput("contrast", "Contrast:",
-                                 list(
-                                   "Rate Difference" = "RD",
-                                   "Rate Ratio" = "RR",
-                                   "Odds Ratio" = "OR",
-                                   "Proportion" = "p")),
+      radioButtons(inputId = "contrastbin",
+                   label = "Contrast:",
+                   list("Rate Difference" = "RD",
+                        "Rate Ratio" = "RR",
+                        "Odds Ratio" = "OR",
+                        "Proportion" = "p"),
+                   selected = "RD",
+                   inline = TRUE),
     ),
+
     conditionalPanel('input.dist == "poi"',
-                     selectInput("contrast", "Contrast:",
-                                 list("Rate Difference" = "RD",
-                                      "Rate Ratio" = "RR",
-                                      "Proportion" = "p"))
+      radioButtons(inputId = "contrastpoi",
+                  label = "Contrast:",
+                  list("Rate Difference" = "RD",
+                       "Rate Ratio" = "RR",
+                      "Event rate" = "p"),
+                  selected = "RD"),
     ),
-    textInput("x1", "x1:", "10"),
-    textInput("n1", "n1:", "100"),
-    conditionalPanel('input.contrast != "p"',
-                     textInput("x2", "x2:", "10"),
-                     textInput("n2", "n2:", "100"),
+
+    textInput("x1", "x1:", "1"),
+    textInput("n1", "n1:", "10"),
+#    conditionalPanel('input.dist == "bin"',
+      conditionalPanel('(input.dist == "bin" & input.contrastbin != "p") |
+                       (input.dist == "poi" & input.contrastpoi != "p")',
+        textInput("x2", "x2:", "1"),
+        textInput("n2", "n2:", "10"),
+      ),
+#      conditionalPanel('(input.dist == "bin" & input.contrastbin == "p") |
+#                       (input.dist == "poi" & input.contrastpoi == "p")',
+#                 textInput("x", "x:", "1"),
+#                 textInput("n", "n:", "10"),
+#      ),
+#    ),
+
+#    conditionalPanel('input.dist == "bin" & input.contrastbin == "p" ||
+#                     input.dist == "poi" & input.contrastpoi == "p"',
+#                     textInput("x", "x:", "1"),
+#                     textInput("n", "n:", "10"),
+#    ),
+
+    conditionalPanel('(input.dist == "bin" & input.contrastbin != "p") |
+                       (input.dist == "poi" & input.contrastpoi != "p")',
+                 strong("Skewness correction (omit for Miettinen-Nurminen method):")
     ),
-    textInput("level", "Confidence level (%):", "95"),
-    selectInput("skew", "Skewness correction:", list("TRUE", "FALSE")),
-    conditionalPanel('input.dist == "bin" && input.contrast != "p"',
-      selectInput("bcf", "'N-1' correction:", list("TRUE", "FALSE"))
+    conditionalPanel('(input.dist == "bin" & input.contrastbin == "p")',
+                 strong("Skewness correction (omit for Wilson Score method):")
     ),
+    conditionalPanel('(input.dist == "poi" & input.contrastpoi == "p")',
+                 strong("Skewness correction (omit for Rao Score method):")
+    ),
+    radioButtons("skew",
+                     NULL,
+                     list("Yes" = "TRUE",
+                          "No" = "FALSE"),
+                      selected = "TRUE",
+                      inline = TRUE),
+
+    conditionalPanel('(input.dist == "bin" & input.contrastbin != "p")',
+
+      conditionalPanel('input.skew == "TRUE"',
+                        strong("'N-1' correction (omit for Gart-Nam method):")
+      ),
+      conditionalPanel('(input.skew == "FALSE" & input.contrastbin == "RD")',
+                       strong("'N-1' correction (omit for Mee method):")
+      ),
+      conditionalPanel('(input.skew == "FALSE" & input.contrastbin == "RR")',
+                       strong("'N-1' correction (omit for Koopman method):")
+      ),
+      conditionalPanel('(input.skew == "FALSE" & input.contrastbin == "OR")',
+                       strong("'N-1' correction (omit for uncorrected Asymptotic Score method):")
+      ),
+      radioButtons("bcf",
+                     label = NULL,
+                     list("Yes" = "TRUE",
+                          "No" = "FALSE"),
+                      selected = "TRUE",
+                      inline = TRUE
+                     ),
+    ),
+
+#        textInput("level", "Confidence level (%):", "95"),
+
     textInput("precis", "Decimal precision:", "4"),
     width = 3
 
+
   ),
+
 
   mainPanel(
     h3(textOutput("caption1")),
     h3(textOutput("caption2")),
     plotOutput("scorePlot")
   )
+
 )
 
 
-#    conditionalPanel('input.contrast == "p"',
-#                     selectInput("theta0", "Null hypothesis theta:", "0.5")
-#    ),
-#    conditionalPanel('input.contrast == "RD"',
-#                     selectInput("theta0", "Null hypothesis theta:", "0")
-#    ),
-#    conditionalPanel('input.contrast == "RR" || input.contrast == "OR"',
-#                     selectInput("theta0", "Null hypothesis theta:", "1")
-#    ),
+  server <- function(input, output) {
 
 
 
-server <- function(input, output) {
+    formulaText1 <- reactive({
 
-  formulaText1 <- reactive({
-    paste0(as.numeric(input$level), "% CI for ",
-           input$contrast, ": ", input$x1,"/", input$n1,
-           ifelse(input$contrast == "p", ":  ",
-                  paste0(" vs ", input$x2,"/", input$n2, ":  ")))
-  })
+      if (input$dist == "bin") {
+#        x1 <- ifelse(input$contrastbin == "p",
+#                     input$x,
+#                     input$x1)
+#        n1 <- ifelse(input$contrastbin == "p",
+#                     input$n,
+#                     input$n1)
+        paste0(as.numeric(input$level), "% CI for binomial ",
+             input$contrastbin, ": ", input$x1,"/", input$n1,
+             ifelse(input$contrastbin == "p", ":  ",
+                    paste0(" vs ", input$x2,"/", input$n2, ":  ")))
+      } else if (input$dist == "poi") {
+#        x1 <- ifelse(input$contrastpoi == "p",
+#                     input$x,
+#                     input$x1)
+#        n1 <- ifelse(input$contrastpoi == "p",
+#                     input$n,
+#                     input$n1)
+        paste0(as.numeric(input$level), "% CI for Poisson ",
+               input$contrastpoi, ": ", input$x1,"/", input$n1,
+               ifelse(input$contrastpoi == "p", ":  ",
+                      paste0(" vs ", input$x2,"/", input$n2, ":  ")))
+      }
 
+    })
 
-  formulaText2 <- reactive({
-    options(digits = 4)
-    out <- scoreci(x1 = as.numeric(input$x1),
-                   x2 = as.numeric(input$x2),
-                   n1 = as.numeric(input$n1),
-                   n2 = as.numeric(input$n2),
-                   level = as.numeric(input$level) / 100,
-                   dist = input$dist,
-                   contrast = input$contrast,
-                   skew = input$skew,
-                   or_bias = input$skew,
-                   bcf = input$bcf,
-                   #                   theta0 = as.numeric(input$theta0),
-                   plot = FALSE,
-                   precis = as.numeric(input$precis))
-    myci <- paste0("(",
-                   formatC(out$estimates[1],
-                           format = ifelse(input$contrast %in% c("p", "RD"), "f", "fg"),
-                           as.numeric(input$precis),
-                           flag = "#"),
-                   ", ",
-                   formatC(out$estimates[3],
-                           format = ifelse(input$contrast %in% c("p", "RD"), "f", "fg"),
-                           digits = as.numeric(input$precis),
-                           flag = "#"),
-                   ")")
-    paste0(myci)
-  })
-
-  # Return the formula text for printing as a caption
-  output$caption1 <- renderText({
-    formulaText1()
-  })
-
-  output$caption2 <- renderText({
-    formulaText2()
-  })
-
-  # Generate a plot of the requested variable against mpg and only
-  # include outliers if requested
-  output$scorePlot <- renderPlot({
-    scoreci(x1 = as.numeric(input$x1),
-            x2 = as.numeric(input$x2),
-            n1 = as.numeric(input$n1),
-            n2 = as.numeric(input$n2),
-            level = as.numeric(input$level) / 100,
-            dist = input$dist,
-            contrast = input$contrast,
-            skew = input$skew,
-            or_bias = input$skew,
-            bcf = input$bcf,
-            #            theta0 = as.numeric(input$theta0),
-            plot = TRUE,
-            precis = as.numeric(input$precis))
-  })
+    formulaText2 <- reactive({
+      options(digits = 4)
+      contrast <- ifelse(input$dist == "bin",
+                         input$contrastbin,
+                         input$contrastpoi)
+#      x1 <- ifelse(input$contrastbin == "p", input$x, input$x1)
+#      n1 <- ifelse(input$contrastbin == "p", input$n, input$n1)
+      out <- scoreci(x1 = as.numeric(input$x1),
+                     x2 = as.numeric(input$x2),
+                     n1 = as.numeric(input$n1),
+                     n2 = as.numeric(input$n2),
+                     level = as.numeric(input$level) / 100,
+                     dist = input$dist,
+                     contrast = contrast,
+                     skew = input$skew,
+                     or_bias = input$skew,
+                     bcf = input$bcf,
+                     #                   theta0 = as.numeric(input$theta0),
+                     plot = FALSE,
+                     precis = as.numeric(input$precis))
+      myci <- paste0("(",
+                     formatC(out$estimates[1],
+                             format = ifelse(contrast %in% c("p", "RD"), "f", "fg"),
+                             as.numeric(input$precis),
+                             flag = "#"),
+                     ", ",
+                     formatC(out$estimates[3],
+                             format = ifelse(contrast %in% c("p", "RD"), "f", "fg"),
+                             digits = as.numeric(input$precis),
+                             flag = "#"),
+                     ")")
+      paste0(myci)
+    })
 
 
-}
+    # Return the formula text for printing as a caption
+    output$caption1 <- renderText({
+      formulaText1()
+    })
 
-shinyApp(ui = ui, server = server)
+    output$caption2 <- renderText({
+      formulaText2()
+    })
+
+    output$scorePlot <- renderPlot({
+      contrast <- ifelse(input$dist == "bin",
+                         input$contrastbin,
+                         input$contrastpoi)
+      scoreci(x1 = as.numeric(input$x1),
+              x2 = as.numeric(input$x2),
+              n1 = as.numeric(input$n1),
+              n2 = as.numeric(input$n2),
+              level = as.numeric(input$level) / 100,
+              dist = input$dist,
+              contrast = contrast,
+              skew = input$skew,
+              or_bias = input$skew,
+              bcf = input$bcf,
+              #            theta0 = as.numeric(input$theta0),
+              plot = TRUE,
+              precis = as.numeric(input$precis))
+    })
+
+  }
+
+
+  shinyApp(ui = ui, server = server)
+
+
+
